4f5e80e6a138496bec23fb0170fff53a7962d2fe
fkantelberg <florian.kantelberg@initos.com>
2021-06-07 16:43:23 +0200

    Bump minor

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="doblib",
-    version="0.7",
+    version="0.7.1",
     author="initOS GmbH",
     author_email="info@initos.com",
     description="Management tool for Odoo installations",
